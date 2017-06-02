4f78dba0c4314d84c276a0c7ae1ba0a19f655cba
Jeff VanDyke <jeffvd.a216@gmail.com>
2017-06-01 22:39:56 -0400

    Using PropTypes from package 'prop-types' instead of React.PropTypes

--- src/react-bootstrap-toggle.jsx
+++ src/react-bootstrap-toggle.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import * as util from './utils';
 
 const eitherStringOrInteger = PropTypes.oneOfType([
